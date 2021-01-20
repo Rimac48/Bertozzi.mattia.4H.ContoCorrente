190c926490cc6f0436f945edb3527fe9381e0c9b
Rimac48 <mattia.bertozzi@live>
2021-01-20 10:19:30 +0100

    N Commit

---
+++ Bertozzi.mattia.4H.ContoCorrente/Models/CC.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bertozzi.mattia._4H.ContoCorrente.Models
+{
+    class CC
+    {
+        private string _nomecognome;
+        private double _conto;
+        private double _contofinale;
+        private string _numeroCC;
+        private string _descrizione;
+
+        public CC()
+        {
+            _nomecognome = "Mattia Bertozzi";
+            _conto = 15000;
+            _contofinale = _conto;
+            _numeroCC = "XA124359825740";
+            _descrizione = "";
+        }
+
+        public string NomeCognome
+        {
+            get
+            {
+                return _nomecognome;
+            }
+            set
+            {
+                value = _nomecognome;
+            }
+        }
+
+        public double Conto
+        {
+            get
+            {
+                return _conto;
+            }
+            set
+            {
+                value = _conto;
+            }
+        }
+
+        public double ContoFinale
+        {
+            get
+            {
+                return _contofinale;
+            }
+            set
+            {
+                value = _contofinale;
+            }
+        }
+
+        public string NumeroCC
+        {
+            get
+            {
+                return _numeroCC;
+            }
+            set
+            {
+                value = _numeroCC;
+            }
+        }
+
+        public string Descrizione
+        {
+            get
+            {
+                return _descrizione;
+            }
+            set
+            {
+                value = _descrizione;
+            }
+        }
+
+        //METODI
+
+        //serve a aggiungere soldi al CC
+        public string Versamento(double soldi, string info)
+        {
+            double aggiungisoldi = soldi;
+            _contofinale = _contofinale + aggiungisoldi;
+            return Movimenti("+", aggiungisoldi, info);
+
+        }
+
+        //serve a toglire soldi al CC
+        public string Prelievo(double soldi,string info)
+        {
+            double rimuovisoldi = soldi;
+            _contofinale = _contofinale - rimuovisoldi;
+            return Movimenti("-", rimuovisoldi,info);
+        }
+
+
+        public double SaldoIniziale()
+        {
+            return _conto;
+        }
+
+        public double SaldoFinale()
+        {
+            return _contofinale;
+        }
+
+
+        //serve a vedere i movimento effettuati nel CC
+        public string Movimenti(string toglimetti,double soldi,string info)
+        {
+            //anno,mese,giorno
+
+            int anno = 2021;
+            int mese = 01;
+
+            Random rnd = new Random();
+            int rdngiorno = rnd.Next(1,32);
+
+            DateTime Data = new DateTime(anno, mese, rdngiorno);
+            DateTime onlyData = Data.Date;
+
+            if (toglimetti=="+")
+            {
+                Console.WriteLine($"\t{onlyData.ToString("d")} \t+ {soldi} euro\t{info}\n");
+                return "\n";
+                
+            }
+            else if(toglimetti == "-")
+            {
+                Console.WriteLine($"\t{onlyData.ToString("d")} \t- {soldi} euro\t{info}\n");
+                return "\n";
+            }
+
+
+            return "";
+        }
+    }
+}
---
+++ Bertozzi.mattia.4H.ContoCorrente/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Bertozzi.mattia._4H.ContoCorrente.Models;
+
+namespace Bertozzi.mattia._4H.ContoCorrente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CC Utente = new CC();
+
+            Console.WriteLine($"Numero conto corrente: {Utente.NumeroCC}");
+            Console.WriteLine($"Utente: {Utente.NomeCognome}");
+            Console.WriteLine($"Saldo Iniziale: {Utente.SaldoIniziale()}\n");
+
+            Console.WriteLine($"Lista movimenti:\n");
+
+            Utente.Prelievo(49.50,"Ghost of Tsushima");
+            Utente.Versamento(1500,"Stipendio!");
+            Utente.Prelievo(499.90,"Ps5");
+            Utente.Prelievo(2000,"info non disponibili");
+            Utente.Prelievo(249.00,"Monitor Odyssey G5");
+
+            Console.WriteLine($"Saldo Finale: {Utente.SaldoFinale()}");
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
